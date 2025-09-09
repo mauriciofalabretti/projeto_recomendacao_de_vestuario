1f7c17bab7b7cc5669548c14d6e4f4a5a085b84f
mauriciofalabretti <maurihbk@gmail.com>
2025-09-09 09:02:38 -0300

    ajustes na organização da interface

--- app_clima.py
+++ app_clima.py
@@ -3,6 +3,8 @@ from main import previsao_tempo, CityCompleter
 from model import gerar_recomendacao
 from prompt_toolkit.document import Document
 
+
+
 st.title('Olá, mundo!')
 st.write('Bem-vindo ao meu primeiro app com Streamlit.')
 
@@ -13,26 +15,39 @@ documento_usuario = Document(cidade)
 
 if len(cidade) > 0:
     gerador_sugestoes = sugestoes.get_completions(documento_usuario, None)
-    lista_sugestoes = [s.text for s in gerador_sugestoes]
-    lista_cidades = st.selectbox("Sugestões de cidades:",lista_sugestoes)
-    dados_previsao = previsao_tempo(lista_cidades)
-    if "ERRO" not in dados_previsao:
-        st.write(f'\n Previsão do Tempo para {lista_cidades} ')
-        st.write('-' * 50)
-        for i in range(3):  # Laço de repetição de 3 vezes para retornar os 3 dias da API climática
-            data = dados_previsao["forecast"]["forecastday"][i]["date"]
-            temp_min = dados_previsao["forecast"]["forecastday"][i]["day"]["mintemp_c"]
-            temp_max = dados_previsao["forecast"]["forecastday"][i]["day"]["maxtemp_c"]
-            condicao = dados_previsao["forecast"]["forecastday"][i]["day"]["condition"]["text"].strip().lower()
-            recomendacao = gerar_recomendacao(temp_min, temp_max,
-                                              condicao)  # De posse das temperaturas e condição climática, gera uma recomendação para cada um dos dias
 
-            # Estrutura de apresentação dos dados e recomendação
-            st.write(f'\nData: {data}')
-            st.write(f'Temperatura: min {temp_min}° e máx {temp_max}°')
-            st.write(f'Condição: {condicao}\n')
-            st.write(f'-----'
-                  f'\nRecomendação de Vestuário:\n'
-                  f'{recomendacao}')
-    else:  # Se houver "ERRO" dentro de `dados_previsao`, vai imprimir a mensagem que a variável guarda
-        st.write(dados_previsao)
+    if gerador_sugestoes is not None:
+        lista_sugestoes = [s.text for s in gerador_sugestoes]
+
+
+        st.write("Sugestões de cidades:")
+        for i, sugestao in enumerate(lista_sugestoes):
+            if st.button(sugestao, key=f'{sugestao}_{i}'):
+                st.session_state['cidade_escolhida'] = sugestao
+                st.markdown(f"<span style ='color:aquamarine'>Você escolheu: {sugestao}</span>", unsafe_allow_html=True)
+
+    if 'cidade_escolhida' in st.session_state:
+        dados_previsao = previsao_tempo(st.session_state.cidade_escolhida)
+
+        if "ERRO" not in dados_previsao:
+            st.divider()
+            st.write(f'\n Previsão do Tempo para {st.session_state.cidade_escolhida} ')
+            for i in range(3):  # Laço de repetição de 3 vezes para retornar os 3 dias da API climática
+                data = dados_previsao["forecast"]["forecastday"][i]["date"]
+                temp_min = dados_previsao["forecast"]["forecastday"][i]["day"]["mintemp_c"]
+                temp_max = dados_previsao["forecast"]["forecastday"][i]["day"]["maxtemp_c"]
+                condicao = dados_previsao["forecast"]["forecastday"][i]["day"]["condition"]["text"].strip().lower()
+
+                recomendacao = gerar_recomendacao(temp_min, temp_max,
+                                                  condicao)  # De posse das temperaturas e condição climática, gera uma recomendação para cada um dos dias
+
+                # Estrutura de apresentação dos dados e recomendação
+                st.divider()
+                st.write(f'Data: {data}')
+                st.write(f'Temperatura: min {temp_min}° e máx {temp_max}°')
+                st.write(f'Condição: {condicao}')
+                st.write(f'-----'
+                      f'\nRecomendação de Vestuário:\n'
+                      f'{recomendacao}')
+        else:  # Se houver "ERRO" dentro de `dados_previsao`, vai imprimir a mensagem que a variável guarda
+            st.write(dados_previsao)
